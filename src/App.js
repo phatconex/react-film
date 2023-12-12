f9be24ebd4f066812d9d48cbd1693bf4e3115b6b
truknguyen <trucng97@gmail.com>
2023-12-13 00:35:03 +0700

    fix app.css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./component/Header/Header";
 import Footer from "./component/Footer/Footer";
 import Home from "./component/Home/Home";
 import { Routes, Route } from "react-router-dom";
-import "./app.css";
+import "./App.css";
 import ListMovie from "./component/ListMovie/ListMovie";
 import DetailMovie from "./component/DetailMovie/DetailMovie";
 import { ApiProvider } from "./component/ApiContext";
